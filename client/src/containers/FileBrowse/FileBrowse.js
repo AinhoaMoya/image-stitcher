533819be99d58dceba1392571218122fda92da0f
Ainhoa Moya <ainhoa.moya91@gmail.com>
2018-07-02 14:19:06 -0700

    Fix FileBrowse bug

--- client/src/containers/FileBrowse/FileBrowse.js
+++ client/src/containers/FileBrowse/FileBrowse.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import * as imageActions from '../../actions/imageActions';
 import { bindActionCreators } from 'redux';
 import getPreviewImages from '../../helpers/getPreviewImages';
-import getImgFormData from '../../helpers/getImgFormData';
 
 function mapStateToProps(state) {
   state = state.images;
@@ -25,39 +24,48 @@ function mapDispatchToProps(dispatch) {
 
 class FileBrowse extends Component {
 
-  handleImages(images) {
-    let previewImages = getPreviewImages(images);
-    let formData = getImgFormData(images);
+  static getDerivedStateFromProps(nextProps, prevState){
+    let images = nextProps.images;
+    if (nextProps.imgCounter === 4) {
+      let formData = new FormData();
 
-    axios.post('/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
+      for (var i = 0; i < images.length; i++) {
+        let image = images[i].imgObject;
+        formData.append(`browsedImage${i}`, image)
       }
-    })
-    .then((response) => {
-      console.log('Successfully uploaded image to server!');
-      this.props.imageActions.addImages(previewImages);
-      this.props.imageActions.setMergedImg(response.data.imgUrl);
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+
+      axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then((response) => {
+        console.log('Successfully uploaded image to server!');
+        nextProps.imageActions.setMergedImg(response.data.imgUrl);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }
   }
 
+  async inputHandler(e) {
 
-  uploadHandler(e) {
-    let inputImages = e.target.files;
-    console.log(inputImages)
-    let previewImages = getPreviewImages(inputImages);
-    if (inputImages.length > 4 || inputImages.length > (4 - this.props.imgCounter)) {
+    let images = e.target.files;
+
+    if (images.length > 4 || images.length > (4 - this.props.imgCounter)) {
       e.target.value = null;
       alert('There is a 4 images limit!')
-    } else if (inputImages.length === 4 || inputImages.length === (4 - this.props.imgCounter)) {
-      this.props.imageActions.increaseCounter(inputImages.length);
-      this.handleImages(inputImages);
     } else {
-      this.props.imageActions.addImages(previewImages);
-      this.props.imageActions.increaseCounter(inputImages.length);
+      let props = this.props;
+      // TODO: REMOVE SETTIMEOUT HACK AND FIX
+      getPreviewImages(images, props).then((images) => {
+        setTimeout(function(){
+          images.map((image) => {
+            props.imageActions.addImages(image);
+          })
+        }, 1000);
+      })
     }
   }
 
@@ -66,7 +74,7 @@ class FileBrowse extends Component {
       <div className="fileBrowse">
         <form>
           <label htmlFor="imageUploader">Choose up to 4 images</label>
-            <input type="file" name="imageUploader" multiple accept="image/*" size="60" onChange={this.uploadHandler.bind(this)} />
+            <input type="file" name="imageUploader" multiple accept="image/*" size="60" onChange={this.inputHandler.bind(this)} />
         </form>
       </div>
     );
